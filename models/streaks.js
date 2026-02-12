808aee8dde64e660ebd9beaab97420f724322a67
Akanksha C <akankshaprajwalbhat@gmail.com>
2026-02-12 16:58:53 +0530

    chore: added models

---
+++ models/streaks.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const streakSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    currentStreak: {
+      type: Number,
+      default: 0,
+    },
+    lastCompleted: {
+      type: Date,
+    },
+  },
+  { timestamps: true },
+);
+
+((module.exports = mongoose), model("Streak", streakSchema));
