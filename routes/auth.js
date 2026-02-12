1e2be79336c356426a49e170be8ed59b1b53d96e
Akanksha C <akankshaprajwalbhat@gmail.com>
2026-02-12 17:23:42 +0530

    chore: added routes

---
+++ routes/auth.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const router = express.Router();
+
+router.post("/signup", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+    const hashedPwd = await bcrypt.hash(password, 10);
+
+    const user = new User({ username, email, password: hashedPwd });
+
+    await user.save();
+    res.status(201).json({ message: "User signed up" });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    const validPwd = await bcrypt.compare(password, user.password);
+    if (!validPwd) {
+      return res.status(400).json({ message: "Invalid Password" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.status(200).json({ token, message: "User logged in" });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+module.exports = router;
---
+++ routes/streaks.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const Streak = require("../models/Streak");
+const auth = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+router.post("/", auth, async (req, res) => {
+  const { title } = req.body;
+
+  const streak = new Streak({
+    user: req.user.id,
+    title,
+  });
+
+  await streak.save();
+  res.json(streak);
+});
+
+router.get("/", auth, async (req, res) => {
+  const streaks = await Streak.find({ user: req.user.id });
+  res.json(streaks);
+});
+
+router.put("/:id/done", auth, async (req, res) => {
+  const streak = await Streak.findById(req.params.id);
+
+  const today = new Date();
+  const yesterday = new Date();
+  yesterday.setDate(today.getDate() - 1);
+
+  if (
+    streak.lastCompleted &&
+    streak.lastCompleted.toDateString() === yesterday.toDateString()
+  ) {
+    streak.currentStreak += 1;
+  } else {
+    streak.currentStreak = 1;
+  }
+
+  streak.lastCompleted = today;
+
+  await streak.save();
+  res.json(streak);
+});
+
+module.exports = router;
